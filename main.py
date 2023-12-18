f24516ded807f9a627e2f3b2e3814c1efeb72881
pramodyasahan <sahandil1013@gmail.com>
2023-12-18 16:17:38 +0530

    feat: apply column transform to features

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 dataset = pd.read_csv('car.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0, 1, 4])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
