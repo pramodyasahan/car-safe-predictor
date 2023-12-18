d6184a7528dcac72c6022eb7b2a95797a060872f
pramodyasahan <sahandil1013@gmail.com>
2023-12-18 16:10:06 +0530

    feat: import the libraries

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, accuracy_score
