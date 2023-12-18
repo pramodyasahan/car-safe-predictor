792217282762149613b4d01fd493c924753c6240
pramodyasahan <sahandil1013@gmail.com>
2023-12-18 16:26:21 +0530

    feat: check accuracy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.neighbors import KNeighborsClassifier
@@ -11,14 +10,22 @@ dataset = pd.read_csv('car.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0, 1, 4])], remainder='passthrough')
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0, 1, 2, 3, 4])], remainder='passthrough',
+                       sparse_threshold=0)
 X = np.array(ct.fit_transform(X))
-
+print(X)
 le = LabelEncoder()
 y = le.fit_transform(y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-classifier = KNeighborsClassifier(n_neighbors=5, metric='euclidean')
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski')
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
+
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
