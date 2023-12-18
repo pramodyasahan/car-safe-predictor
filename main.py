30bbb3afbb7b1ef54a62b0b9c911c25c31d869dc
pramodyasahan <sahandil1013@gmail.com>
2023-12-18 16:12:25 +0530

    feat: import the dataset

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.metrics import confusion_matrix, accuracy_score
+
+dataset = pd.read_csv('car.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
